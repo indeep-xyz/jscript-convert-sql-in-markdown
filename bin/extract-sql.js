21896ddb781bd339ba19582ad612a7a8b130a279
indeep-xyz@gmail.com <indeep.xyz@gmail.com>
2016-10-23 22:06:00 +0900

    Add an error in extract-sql.js
    It raises when that does not find SQL statement in Markdown.

--- bin/extract-sql.js
+++ bin/extract-sql.js
@@ -8,12 +8,20 @@ var options = {
 // main
 
 var fileList = FileObject.createObjects();
+var sqlCount = 0;
 
 for (var i = 0; i < fileList.length; i++) {
   var file = fileList[i];
   var sqlObject = new SqlObject(file.text);
 
   if (sqlObject.hasSql) {
+    sqlCount++;
     file.write(file.path + '.sql', sqlObject.sql);
   }
+}
+
+if (sqlCount < 1) {
+  WScript.Echo(
+      'Did not find SQL statement');
+  WScript.Quit(1);
 }
